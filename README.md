0c06705c8d3e28fe7f0a15a5cab044a5a5a34a09
Max Wällstedt <max.wallstedt@foreseeti.com>
2021-07-08 10:20:54 +0200

    Apply the apache license

---
+++ README.md
@@ -0,0 +1,10 @@
+# securicad-coa-generator
+
+Description
+
+## License
+
+Copyright © 2020-2021 [Wojciech Wideł](mailto:widel@kth.se)
+Copyright © 2021 [Preetam Mukherjee](mailto:preetam@kth.se)
+
+Licensed under the [Apache License, Version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
--- securicad/coagen.py
+++ securicad/coagen.py
@@ -1,3 +1,17 @@
+# Copyright 2020-2021 Wojciech Wideł <widel@kth.se>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from securicad.model import Model
 from securicad.coa import CoA
 from securicad.attackg import AttackGraph, merge_attack_graphs
